983218aaaab304eabc39f9b1efbeae22d90b7b62
gagliano-kevin <gagliano.kevin.97@gmail.com>
2025-10-22 21:35:58 +0200

    ...

--- astar_optimization/simple_dataset_test/sine_test.py
+++ astar_optimization/simple_dataset_test/sine_test.py
@@ -18,7 +18,7 @@ NOISE_LEVEL = 0.1
 X_sin, Y_sin = generate_sinusoidal_tensor(func=torch.sin, num_samples=NUM_SAMPLES, min_angle=MIN_ANGLE, max_angle=MAX_ANGLE, noise_level=NOISE_LEVEL)
 
 model = nn.Sequential(
-    nn.Linear(1, 18),  
+    nn.Linear(1, 8),  
     nn.ReLU(),
     #nn.Tanh(),
     nn.Linear(8, 8),
@@ -28,7 +28,7 @@ model = nn.Sequential(
     #nn.Tanh()
 )
 
-trainer = Trainer(model, nn.MSELoss(), quantization_factor=10, parameter_range=(-4, 4), debug_mlp=True, param_fraction=1.0, max_iterations=1000, log_freq=10, target_loss=0.0001, update_strategy=2, g_ini_val=0, g_step=0.01, alpha=0.5, scale_f=True)
+trainer = Trainer(model, nn.MSELoss(), quantization_factor=5, parameter_range=(-4, 4), debug_mlp=True, param_fraction=1.0, max_iterations=4000, log_freq=100, target_loss=0.01, update_strategy=2, g_ini_val=0, g_step=0.01, alpha=0.5, scale_f=True)
 
 trainer.train(X_sin, Y_sin)
 

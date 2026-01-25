db23b97f74d488adf41e078ce3442643852bd2e4
gagliano-kevin <gagliano.kevin.97@gmail.com>
2026-01-25 21:24:32 +0100

    added a new comparison test for neighbors generation with beam search. The parameter for random sampling need to be searched via a grid search. For this purpose a grid_search was designed unically for random sampling method

---
+++ tests/neighbors_generation_strategies/grid_search_random_sampling/california_housing.py
@@ -0,0 +1,304 @@
+#==============================================================================================================================================================
+#==============================================================================================================================================================
+# python -u -m tests.neighbors_generation_strategies.grid_search_random_sampling.california_housing |& tee tests/neighbors_generation_strategies/grid_search_random_sampling/california_housing_output.txt 
+#==============================================================================================================================================================
+#==============================================================================================================================================================
+
+#==============================================================================================================================================================
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#                                                       Test on California Housing Dataset to compare:
+#                     Single Kernel Neighbor Generation vs Layer-Wise Kernels Neighbor Generation vs Random Sampling Neighbor Generation 
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#==============================================================================================================================================================
+
+
+import torch.nn as nn
+
+from source.PathNet import Trainer, TrainerLayerWiseKernel, TrainerRandomSampling
+from source.utils.dataset_utils.housing_utils import get_california_housing_data
+from source.utils.evaluation_utils import evaluate_pathnet_regression
+from source.utils.plot_utils import generate_plots, format_sci,  save_metrics, generate_evaluation_statistical_summary, plot_regression_statistics
+
+ITERATIONS = 2000
+RUNS = 5
+SAVE_TRAINED_MODEL = False
+MODEL_NAME_PREFIX = "housing_model"
+DATASET_NAME = "California Housing"
+EARLY_STOPPING = False
+E_S_PATIENCE = 200
+LOSS_IMPROVEMENT_THRESHOLD = 1e-3
+PARAMETER_RANGE = (-10, 10)
+DELTA_ABS = None
+
+
+# =========================================================================================================================================================
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------- SMALL NET --------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+# =========================================================================================================================================================
+
+TEST_NAME = "small_net_random_sampling_grid_search"
+
+# MLP Hyperparameters
+INPUT_SIZE = 8
+HIDDEN_SIZE_1 = 32
+HIDDEN_SIZE_2 = 16
+OUTPUT_SIZE = 1
+
+# Parameters for Random Sampling Neighbors Generation
+PERTURBATION_RATIO = 0.01       # 1% of the parameters will be perturbed per each neighbor
+SEARCH_COVERAGE_RATIO = 0.5     # 5% of the total number of parameters in the model will be the number of neighbors generated per each state
+
+# Quantization Parameter
+QUANTIZATION_FACTOR = 10
+
+labels_list = ["Single Kernel Neighbors Generation", "layer-Wise Kernels Neighbors Generation", "Random Neighbors Generation"]
+
+metrics_list = [
+    {
+        "losses": [],
+        "training_times": [],
+        "final_losses": [],
+        "dynamic_quantization_iterations": [],
+        "dynamic_kernel_reshaping_iterations": [],
+        "evaluation_scores": []
+    } for _ in range(len(labels_list))
+]
+
+X_train, Y_train, X_val, Y_val, X_test, Y_test = get_california_housing_data()
+print(f"\nTraining Data Shape: {X_train.shape}, {Y_train.shape}")
+print(f"Validation Data Shape: {X_val.shape}, {Y_val.shape}")
+print(f"Testing Data Shape: {X_test.shape}, {Y_test.shape}\n")
+
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------- RANDOM SAMPLING NEIGHBORS GENERATION -------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+for run in range(RUNS):
+    print(f"\n--- TEST NAME: {TEST_NAME} \t Random Sampling Neighbors Generation \t Vanilla ASTAR Training Run {run + 1} ---\n")
+    model = nn.Sequential(
+            nn.Linear(INPUT_SIZE, HIDDEN_SIZE_1),  
+            nn.ReLU(),
+            nn.Linear(HIDDEN_SIZE_1, HIDDEN_SIZE_2),
+            nn.ReLU(),
+            nn.Linear(HIDDEN_SIZE_2, OUTPUT_SIZE),
+            )
+
+
+    trainer = TrainerRandomSampling(model=model,
+                            loss_fn=nn.MSELoss(),
+                            quantization_factor=QUANTIZATION_FACTOR,
+                            parameter_range=PARAMETER_RANGE,
+                            debug_mlp=False,
+                            #----------------------------------------------------------------------------------
+                            perturbation_ratio=PERTURBATION_RATIO, search_coverage_ratio=SEARCH_COVERAGE_RATIO,
+                            delta_abs=DELTA_ABS,
+                            #----------------------------------------------------------------------------------
+                            early_stopping=EARLY_STOPPING, e_s_patience=E_S_PATIENCE,
+                            #----------------------------------------------------------------------------------
+                            dynamic_quantization=False,
+                            #----------------------------------------------------------------------------------
+                            loss_improvement_threshold=LOSS_IMPROVEMENT_THRESHOLD,
+                            #----------------------------------------------------------------------------------
+                            max_iterations=ITERATIONS, log_freq=100, measure_time=True, save_trained_model=SAVE_TRAINED_MODEL, model_name=MODEL_NAME_PREFIX + f'_random_sampling_astar_run_{run + 1}'
+                            )
+
+    trainer.train(X_train, Y_train)
+
+    metrics_list[2]["losses"].append(trainer.loss_history)
+    metrics_list[2]["training_times"].append(trainer.training_time)
+    metrics_list[2]["final_losses"].append(trainer.best_node.h_val)
+    metrics_list[2]["dynamic_quantization_iterations"].append(trainer.dynamic_adjustments_log["dynamic_quantization_iterations"])
+    metrics_list[2]["evaluation_scores"].append(evaluate_pathnet_regression(trainer, (X_test, Y_test)))
+
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------- COMPARISON ---------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+all_results = {label: metric for label, metric in zip(labels_list, metrics_list)}
+
+save_metrics(all_results, TEST_NAME)
+
+generate_evaluation_statistical_summary(metrics_list,labels_list, TEST_NAME)
+
+generate_plots(metrics_list, labels_list, TEST_NAME, DATASET_NAME)
+
+plot_regression_statistics(metrics_list, labels_list, TEST_NAME, DATASET_NAME)
+
+
+
+# =========================================================================================================================================================
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------- MEDIUM NET --------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+# =========================================================================================================================================================
+
+TEST_NAME = "medium_net_random_sampling_grid_search"
+
+# MLP Hyperparameters
+HIDDEN_SIZE_1 = 64
+HIDDEN_SIZE_2 = 32
+HIDDEN_SIZE_3 = 16
+
+labels_list = ["Single Kernel Neighbors Generation", "layer-Wise Kernels Neighbors Generation", "Random Neighbors Generation"]
+
+metrics_list = [
+    {
+        "losses": [],
+        "training_times": [],
+        "final_losses": [],
+        "dynamic_quantization_iterations": [],
+        "dynamic_kernel_reshaping_iterations": [],
+        "evaluation_scores": []
+    } for _ in range(len(labels_list))
+]
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------- RANDOM SAMPLING NEIGHBORS GENERATION -------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+for run in range(RUNS):
+    print(f"\n--- TEST NAME: {TEST_NAME} \t Random Sampling Neighbors Generation \t Vanilla ASTAR Training Run {run + 1} ---\n")
+
+    model = nn.Sequential(
+            nn.Linear(INPUT_SIZE, HIDDEN_SIZE_1),  
+            nn.ReLU(),
+            nn.Linear(HIDDEN_SIZE_1, HIDDEN_SIZE_2),
+            nn.ReLU(),
+            nn.Linear(HIDDEN_SIZE_2, HIDDEN_SIZE_3),
+            nn.ReLU(),
+            nn.Linear(HIDDEN_SIZE_3, OUTPUT_SIZE),
+            )
+
+
+    trainer = TrainerRandomSampling(model=model,
+                            loss_fn=nn.MSELoss(),
+                            quantization_factor=QUANTIZATION_FACTOR,
+                            parameter_range=PARAMETER_RANGE,
+                            debug_mlp=False,
+                            #----------------------------------------------------------------------------------
+                            perturbation_ratio=PERTURBATION_RATIO, search_coverage_ratio=SEARCH_COVERAGE_RATIO,
+                            delta_abs=DELTA_ABS,
+                            #----------------------------------------------------------------------------------
+                            early_stopping=EARLY_STOPPING, e_s_patience=E_S_PATIENCE,
+                            #----------------------------------------------------------------------------------
+                            dynamic_quantization=False,
+                            #----------------------------------------------------------------------------------
+                            loss_improvement_threshold=LOSS_IMPROVEMENT_THRESHOLD,
+                            #----------------------------------------------------------------------------------
+                            max_iterations=ITERATIONS, log_freq=100, measure_time=True, save_trained_model=SAVE_TRAINED_MODEL, model_name=MODEL_NAME_PREFIX + f'_random_sampling_astar_run_{run + 1}'
+                            )
+
+    trainer.train(X_train, Y_train)
+
+    metrics_list[2]["losses"].append(trainer.loss_history)
+    metrics_list[2]["training_times"].append(trainer.training_time)
+    metrics_list[2]["final_losses"].append(trainer.best_node.h_val)
+    metrics_list[2]["dynamic_quantization_iterations"].append(trainer.dynamic_adjustments_log["dynamic_quantization_iterations"])
+    metrics_list[2]["evaluation_scores"].append(evaluate_pathnet_regression(trainer, (X_test, Y_test)))
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------- COMPARISON ---------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+all_results = {label: metric for label, metric in zip(labels_list, metrics_list)}
+
+save_metrics(all_results, TEST_NAME)
+
+generate_evaluation_statistical_summary(metrics_list,labels_list, TEST_NAME)
+
+generate_plots(metrics_list, labels_list, TEST_NAME, DATASET_NAME)
+
+plot_regression_statistics(metrics_list, labels_list, TEST_NAME, DATASET_NAME)
+
+
+
+# =========================================================================================================================================================
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------- BIG NET ----------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+# =========================================================================================================================================================
+
+TEST_NAME = "big_net_random_sampling_grid_search"
+# MLP Hyperparameters
+HIDDEN_SIZE_1 = 128
+HIDDEN_SIZE_2 = 64
+HIDDEN_SIZE_3 = 32
+HIDDEN_SIZE_4 = 16
+
+labels_list = ["Single Kernel Neighbors Generation", "layer-Wise Kernels Neighbors Generation", "Random Neighbors Generation"]
+
+metrics_list = [
+    {
+        "losses": [],
+        "training_times": [],
+        "final_losses": [],
+        "dynamic_quantization_iterations": [],
+        "dynamic_kernel_reshaping_iterations": [],
+        "evaluation_scores": []
+    } for _ in range(len(labels_list))
+]
+
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------- RANDOM SAMPLING NEIGHBORS GENERATION -------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+for run in range(RUNS):
+    print(f"\n--- TEST NAME: {TEST_NAME} \t Random Sampling Neighbors Generation \t Vanilla ASTAR Training Run {run + 1} ---\n")
+
+    model = nn.Sequential(
+            nn.Linear(INPUT_SIZE, HIDDEN_SIZE_1),  
+            nn.ReLU(),
+            nn.Linear(HIDDEN_SIZE_1, HIDDEN_SIZE_2),
+            nn.ReLU(),
+            nn.Linear(HIDDEN_SIZE_2, HIDDEN_SIZE_3),
+            nn.ReLU(),
+            nn.Linear(HIDDEN_SIZE_3, HIDDEN_SIZE_4),
+            nn.ReLU(),
+            nn.Linear(HIDDEN_SIZE_4, OUTPUT_SIZE),
+            )
+
+
+    trainer = TrainerRandomSampling(model=model,
+                            loss_fn=nn.MSELoss(),
+                            quantization_factor=QUANTIZATION_FACTOR,
+                            parameter_range=PARAMETER_RANGE,
+                            debug_mlp=False,
+                            #----------------------------------------------------------------------------------
+                            perturbation_ratio=PERTURBATION_RATIO, search_coverage_ratio=SEARCH_COVERAGE_RATIO,
+                            delta_abs=DELTA_ABS,
+                            #----------------------------------------------------------------------------------
+                            early_stopping=EARLY_STOPPING, e_s_patience=E_S_PATIENCE,
+                            #----------------------------------------------------------------------------------
+                            dynamic_quantization=False,
+                            #----------------------------------------------------------------------------------
+                            loss_improvement_threshold=LOSS_IMPROVEMENT_THRESHOLD,
+                            #----------------------------------------------------------------------------------
+                            max_iterations=ITERATIONS, log_freq=100, measure_time=True, save_trained_model=SAVE_TRAINED_MODEL, model_name=MODEL_NAME_PREFIX + f'_random_sampling_astar_run_{run + 1}'
+                            )
+
+    trainer.train(X_train, Y_train)
+
+    metrics_list[2]["losses"].append(trainer.loss_history)
+    metrics_list[2]["training_times"].append(trainer.training_time)
+    metrics_list[2]["final_losses"].append(trainer.best_node.h_val)
+    metrics_list[2]["dynamic_quantization_iterations"].append(trainer.dynamic_adjustments_log["dynamic_quantization_iterations"])
+    metrics_list[2]["evaluation_scores"].append(evaluate_pathnet_regression(trainer, (X_test, Y_test)))
+
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------- COMPARISON ---------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+all_results = {label: metric for label, metric in zip(labels_list, metrics_list)}
+
+save_metrics(all_results, TEST_NAME)
+
+generate_evaluation_statistical_summary(metrics_list,labels_list, TEST_NAME)
+
+generate_plots(metrics_list, labels_list, TEST_NAME, DATASET_NAME)
+
+plot_regression_statistics(metrics_list, labels_list, TEST_NAME, DATASET_NAME)

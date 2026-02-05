ba0d9c1e1a9312ae5d605406cb2d3aa179abe5b4
gagliano-kevin <gagliano.kevin.97@gmail.com>
2026-02-05 23:14:57 +0100

    all results directories are now adjusted with Adam label isntead of sgd

--- source/utils/adjust_plots.py
+++ source/utils/adjust_plots.py
@@ -8,18 +8,17 @@
 from source.utils.plot_utils import generate_plots, generate_evaluation_statistical_summary, plot_regression_statistics, load_metrics, json_to_plot_format, format_sci
 
 DATASET_NAME = "California Housing"
-#TEST_NAME = "small_net_neighbors_generation_strategies_comparison_with_BS"
-#TEST_NAME = "medium_net_neighbors_generation_strategies_comparison_with_BS"
-TEST_NAME = "big_net_neighbors_generation_strategies_comparison_with_BS"
+TEST_NAME = "small_net_california_housing_SGD_vs_A-star"
+NEW_TEST_NAME = "small_net_california_housing_Adam_vs_A-star"
 
 
-labels_list = ["Single Kernel Neighbors Generation", "layer-Wise Kernels Neighbors Generation", "Random Neighbors Generation"]
+labels_list = ["A-star Single Kernel", "A-star Layer-Wise Kernels", "A-star Random Sampling", "Adam"]
 
 metrics_list = json_to_plot_format(load_metrics(TEST_NAME, directory="results"))
 
-generate_evaluation_statistical_summary(metrics_list,labels_list, TEST_NAME)
+generate_evaluation_statistical_summary(metrics_list,labels_list, NEW_TEST_NAME)
 
-generate_plots(metrics_list, labels_list, TEST_NAME, DATASET_NAME)
+generate_plots(metrics_list, labels_list, NEW_TEST_NAME, DATASET_NAME)
 
-plot_regression_statistics(metrics_list, labels_list, TEST_NAME, DATASET_NAME)
+plot_regression_statistics(metrics_list, labels_list, NEW_TEST_NAME, DATASET_NAME)
 
